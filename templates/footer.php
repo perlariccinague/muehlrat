91ac64955b7bce4076ae4b2e0fdc21cef97a157d
PN <pn@klapproth-koch.de>
2023-07-26 07:58:31 +0200

    contain Home and Footer

---
+++ templates/footer.php
@@ -0,0 +1,19 @@
+<div class="footer"
+    <div class="footer-middle">
+        <div class="footer-middle-left">
+            {{insert_content::39}}
+        </div>
+        <div class="footer-middle-new">
+            {{insert_content::40}}
+            {{insert_content::41}}
+            {{insert_content::42}}
+        </div>
+        <div class="footer-middle-right">
+            {{insert_content::43}}
+            {{insert_content::44}}
+        </div>
+    </div>
+</div>
+<div class="footer-bottom">
+    {{insert_module::11}}
+</div>
