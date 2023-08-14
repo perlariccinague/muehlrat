8205ec8e8e2dd58535468f9cb0119c6d94362d00
PN <pn@klapproth-koch.de>
2023-08-14 07:33:16 +0200

    contain Unterseite

---
+++ templates/rsce_my_block_config.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'label' => array('Phase', 'Meine Beschreibung...'),
+    'types' => array('content', 'module'),
+    'contentCategory' => 'texts',
+    'moduleCategory' => 'miscellaneous',
+    'beTemplate' => 'be_wildcard',
+    'standardFields' => array('headline', 'cssID'),
+    'wrapper' => array(
+        'type' => 'none',
+    ),
+    'fields' => array(
+        'image' => array(
+            'label' => array('Bild', 'Beschreibung...'),
+            'eval' => array('filesOnly' => true),
+            'inputType' => 'fileTree'
+        ),
+        'titel' => array(
+            'label' => array('Überschrift', 'Beschreibung...'),
+            'inputType' => 'text',
+        ),
+
+        'description' => array(
+            'label' => array('Text', 'Beschreibung...'),
+            'eval' => array('rte' => 'tinyMCE'),
+            'inputType' => 'textarea',
+        ),
+    ),
+);
