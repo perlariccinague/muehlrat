ff3097408360dc4e4f42739f9c149adfcc889c84
PN <pn@klapproth-koch.de>
2023-08-17 08:14:39 +0200

    mobile styling, menu and search function

--- files/assets/js/menu.js
+++ files/assets/js/menu.js
@@ -1,11 +1,9 @@
 const menu = document.querySelector('.mobile_menu');
 const menuButton = document.querySelector('.mobile_menu_trigger');
 const bg = document.querySelector('.bg');
-/*const wrapper = document.querySelector('#wrapper');*/
-console.log(menuButton);
 let blocked = false;
+
 menuButton.addEventListener('mouseover', function () {
-    console.log("fa");
     menuButton.click();
     blocked = true;
     bg.style.height = "400px"
@@ -13,12 +11,26 @@ menuButton.addEventListener('mouseover', function () {
         blocked = false;
     },500);
 });
-menu.addEventListener('mouseout', function () {
-    console.log("test");
+menu.addEventListener('mouseleave', function () {
     if(!blocked) {
         document.querySelector('.menu-close').click();
         bg.style.height = "160px"
     }
 });
+
+//SEARCH
+
+const searchBtn = document.querySelector(".search");
+const input = document.querySelector(".search_input");
+console.log(input);
+
+console.log(searchBtn);
+
+searchBtn.addEventListener('mouseover', function () {
+    input.style.display = "block";
+    console.log("test");
+
+});
+
 
 
